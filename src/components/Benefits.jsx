4ea2fa4eabd18bb4310ec19a55b1d7620a32b308
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-05-09 19:55:35 +0500

    Working on Benefits section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,7 +1,8 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
-
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
 const Benefits = () => {
   return (
     <Section id="features">
@@ -17,9 +18,36 @@ const Benefits = () => {
               style={{ backgroundImage: `url(${item.backgroundUrl})` }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem]">
-                <h5>{item.title}</h5>
-                <h2>{item.text}</h2>
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h2 mb-5">{item.title}</h5>
+                <p className="body-2 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto uppercase tracking-wider font-bold font-code text-n-1">
+                    Explore More
+                  </p>
+                  <Arrow />
+                </div>
+              </div>
+              {item.light && <GradientLight />}
+              <div
+                className="absolute bg-n-8 inset-0.5"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="">
+                  {item.imageUrl && (
+                    <img
+                      className="rounded-2xl top-1 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+                      src={item.imageUrl}
+                      alt={item.title}
+                    />
+                  )}
+                </div>
               </div>
             </div>
           ))}
