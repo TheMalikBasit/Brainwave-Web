31b6f8a726066556115374e90385d49a835e87bc
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-05-10 21:28:33 +0500

    Almost Completed Features and Collab section

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,21 +1,80 @@
+import { brainwave, brainwaveSymbol, check } from "../assets";
+import { collabApps, collabContent, collabText } from "../constants";
 import Section from "./Section";
-import { BottomLine } from "./design/Hero";
+import Button from "./Button";
+import { LeftCurve, RightCurve } from "./design/Collaboration";
+
 const Collaboration = () => {
   return (
-    <Section
-      className="pt-[12rem] -mt-[5.25rem]"
-      crosses
-      crossesOffset="lg:translate-y-[5.25rem]"
-      customPadding
-      id="#Collaboration"
-    >
-      <div className="relative container z-2">
-        <div className="flex flex-row justify-between">
-          <p>AI Chat App for seamless collaboration</p>
-          <p>
-            With smart automation and top-notch security, it's the perfect
-            solution for teams looking to work smarter.
+    <Section crosses>
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+
+          <ul className="max-w-[24rem] mb-10 md:mb-14">
+            {collabContent.map((item) => (
+              <li className="mb-3 py-3" key={item.id}>
+                <div className="flex items-center">
+                  <img src={check} width={24} height={24} alt={check} />
+                  <h6 className="body-2 ml-5">{item.title}</h6>
+                </div>
+                {item.text && (
+                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                )}
+              </li>
+            ))}
+          </ul>
+
+          <Button>Try it now</Button>
+        </div>
+        <div className="lg:ml-auto xl:w-[38rem] mt-4">
+          <p className="body-2 mb-4 text-n-4 mb:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+            {collabText}
           </p>
+
+          <div className="relative left-1/2 w-[22rem] flex rounded-full border border-n-6 aspect-square -translate-x-1/2 scale-75 md:scale-100">
+            <div className="m-auto w-60 flex rounded-full border border-n-6 aspect-square">
+              <div className="w-[6rem] m-auto rounded-full p-[0.2rem] bg-conic-gradient aspect-square">
+                <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                  <img
+                    className="m-auto"
+                    src={brainwaveSymbol}
+                    width={48}
+                    height={48}
+                    alt="brainwave"
+                  />
+                </div>
+              </div>
+            </div>
+            <ul>
+              {collabApps.map((item, i) => (
+                <li
+                  key={item.id}
+                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
+                    i * 45
+                  }`}
+                >
+                  <div
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                      i * 45
+                    }`}
+                  >
+                    <img
+                      className="m-auto"
+                      src={item.icon}
+                      width={item.width}
+                      height={item.height}
+                      alt={item.title}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <LeftCurve />
+            <RightCurve />
+          </div>
         </div>
       </div>
     </Section>
