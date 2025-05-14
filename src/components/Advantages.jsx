e1bfd44643b19197ae11dcc4a68383eb321f50cf
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-05-15 00:47:07 +0500

    Service 3 initiated and 50% done

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -1,8 +1,14 @@
 import Section from "./Section";
 import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
-import { brainwaveServices } from "../constants";
+import { brainwaveServices, brainwaveServicesIcons } from "../constants";
 import Generating from "./Generating";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 const Advantages = () => {
   return (
     <Section id="how-to-use">
@@ -51,7 +57,7 @@ const Advantages = () => {
           </div>
 
           <div className="relative z-1 grid gap-5 lg:grid-cols-2">
-            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
+            <div className="relative min-h-[45rem] border border-n-1/10 rounded-3xl overflow-hidden">
               <div className="absolute inset-0">
                 <img
                   className="w-full h-full object-cover"
@@ -61,14 +67,63 @@ const Advantages = () => {
                   alt="Smart AI 2"
                 />
               </div>
-              <div className="absolute">
-                <h4 className="h4 mb-4">Smartest AI</h4>
+              <div className="absolute inset-0 flex flex-col justify-end p-8 bg-gradient-to-b from-n-8/0 to-n-8/90 lg:p-15">
+                <h4 className="h4 mb-4">Photo editing</h4>
                 <p className="body-2 text-n-3 mb-[3rem]">
-                  Brainwave unlocks the potential of AI-powered applications
+                  Automatically enhance your photos using our AI app's photo
+                  editing feature. Try it now!
                 </p>
+                <PhotoChatMessage />
               </div>
             </div>
-            <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden"></div>
+            <div className="p-4 min-h-[46rem] bg-n-7 rounded-3xl overflow-hidden">
+              <div className="py-12 px-4 xl:px-8 ">
+                <h4 className="h4 mb-4">Video generation</h4>
+                <p className="body-2 text-n-3 mb-[2rem]">
+                  The world’s most powerful AI photo and video art generation
+                  engine. What will you create?
+                </p>
+
+                <ul className="flex items-center justify-between">
+                  {brainwaveServicesIcons.map((item, index) => (
+                    <li
+                      key={index}
+                      className={`rounded-2xl flex items-center justify-center ${
+                        index === 2
+                          ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                          : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                      }`}
+                    >
+                      <div
+                        className={
+                          index === 2
+                            ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                            : ""
+                        }
+                      >
+                        <img
+                          className=""
+                          src={item}
+                          width={30}
+                          height={30}
+                          alt=""
+                        />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="relative">
+                <img
+                  className="w-full h-full object-cover"
+                  src={service3}
+                  width={630}
+                  height={750}
+                  alt="Smart AI 3"
+                />
+              </div>
+              <div className=""></div>
+            </div>
           </div>
         </div>
       </div>
