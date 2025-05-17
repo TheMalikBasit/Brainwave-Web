bbfedf5c7196a53fcabc58f2875671eafd70e164
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-05-17 16:54:56 +0500

    Second Footer Completed (Personal Links)

--- src/components/FooterTwo.jsx
+++ src/components/FooterTwo.jsx
@@ -1,5 +1,31 @@
+import Section from "./Section";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 const FooterTwo = () => {
-  return <div>FooterTwo</div>;
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div className="container flex justify-center sm:justify-between items-center gap-10 max-sm:flex-col">
+        <a
+          className="text-gray font-semibold text-xs flex items-center my-2"
+          href="https://github.com/TheMalikBasit"
+          target="_blank"
+        >
+          <FontAwesomeIcon className="mr-5" icon={faGithub} size="2x" />
+          github.com/TheMalikBasit
+        </a>
+        <a
+          className="text-gray font-semibold text-xs flex items-center my-2"
+          href="https://www.linkedin.com/in/malik-abdul-basit-24b634267/"
+          target="_blank"
+        >
+          <FontAwesomeIcon className="mr-5" icon={faLinkedin} size="2x" />
+          linkedin.com/in/malik-abdul-basit
+        </a>
+      </div>
+    </Section>
+  );
 };
 
 export default FooterTwo;
